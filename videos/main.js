f36043abb90fe8b3e487b9eb736a976b61830b1b
buuparty <105058440+buuparty@users.noreply.github.com>
2022-05-06 19:52:04 +0630

    Add files via upload

---
+++ videos/main.js
@@ -0,0 +1,36 @@
+const Api = "api/videos.json";
+const divTag = document.querySelector(".container");
+const titleTag = document.getElementsByTagName("title");
+
+fetch(Api)
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+     try{
+     	const videoId = new URL(window.location).searchParams.get("id");
+
+      if (videoId <= data.length-1 ){
+      	const i = videoId;
+      	const test =  `
+         <h1>${data[i].title}</h1>`;
+        
+      	divTag.innerHTML += test;
+      	titleTag[0].innerHTML += "Buu Party"+" - "+data[i].title
+      }else {
+      	console.log("error");
+      }
+     }catch{
+      	
+     }
+   })
+   .catch((error) => {
+      console.log("Error"+ error);
+   });
+
+
+
+
+
+
+
